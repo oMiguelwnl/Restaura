5986a8147525137b76d63f84428e7c291c4e5be3
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-30 14:13:31 -0300

    feat: add navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
+    <main className="overflow-y-hidden text-neutral-200 antialiased">
       <HeroSection />
-    </div>
+      <NavBar />
+    </main>
   );
 }
 
