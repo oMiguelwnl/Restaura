9c1bbad3d4957f39fa607e2e877b42e7a0c1350f
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-31 14:57:33 -0300

    feat: add framer-motion animations to HeroSection component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import video from "../assets/hero.mp4";
 import logo from "../assets/logo.png";
 import hero from "../assets/hero.jpeg";
+import { motion } from "framer-motion";
 
 function HeroSection() {
   return (
@@ -18,7 +19,14 @@ function HeroSection() {
       </div>
       <div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent from-70% to-black"></div>
       <div className="relative z-20 flex h-screen flex-col justify-end pb-20">
-        <img src={logo} alt="Restaura" className="w-full p-4" />
+        <motion.img
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          src={logo}
+          alt="Restaura"
+          className="w-full p-4"
+        />
         <p className="p-4 text-lg tracking-tighter text-white">Paris</p>
       </div>
     </section>
