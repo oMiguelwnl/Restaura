1c26fe509a7f6afc247cfce11cb1cf41c501787c
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-31 14:59:14 -0300

    feat: add framer-motion animations to Review component

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -4,15 +4,49 @@ import customer1 from "../assets/customer1.jpeg";
 import customer2 from "../assets/customer2.jpeg";
 import customer3 from "../assets/customer3.jpeg";
 import customer4 from "../assets/customer4.jpeg";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.8,
+    },
+  },
+};
+
+const ItemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 
 const Review = () => {
   return (
     <section className="container mx-auto mb-8 mt-12" id="avaliacoes">
-      <div className="flex flex-col">
-        <p className="mb-10 text-3xl font-light leading-normal tracking-tighter lg:mx-40 lg:mt-40 lg:text-[3.5rem]">
+      <motion.div
+        initial="hidden"
+        whileInView="show"
+        variants={containerVariants}
+        viewport={{ once: true }}
+        className="flex flex-col"
+      >
+        <motion.p
+          variants={ItemVariants}
+          className="mb-10 text-3xl font-light leading-normal tracking-tighter lg:mx-40 lg:mt-40 lg:text-[3.5rem]"
+        >
           {REVIEW.content}
-        </p>
-        <div className="flex items-center justify-center gap-6">
+        </motion.p>
+        <motion.div
+          variants={ItemVariants}
+          className="flex items-center justify-center gap-6"
+        >
           <img
             src={xaviour}
             width={80}
@@ -24,18 +58,25 @@ const Review = () => {
             <h6>{REVIEW.name}</h6>
             <p className="text-sm text-neutral-500">{REVIEW.profession}</p>
           </div>
-        </div>
-      </div>
-      <div className="mt-14 flex flex-col items-center justify-center gap-2 md:flex-row">
+        </motion.div>
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="show"
+        variants={ItemVariants}
+        viewport={{ once: true }}
+        className="mt-14 flex flex-col items-center justify-center gap-2 md:flex-row"
+      >
         {[customer1, customer2, customer3, customer4].map((customer, index) => (
-          <img
+          <motion.img
+            variants={ItemVariants}
             src={customer}
             key={index}
             alt="customer"
             className="h-[300px] w-[200px] rounded-br-3xl rounded-tl-3xl object-cover "
           />
         ))}
-      </div>
+      </motion.div>
     </section>
   );
 };
