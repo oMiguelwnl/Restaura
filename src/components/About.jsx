5e17df2f5d40cba59874bffec3ebbb5c800bb588
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-31 14:54:05 -0300

    feat: add framer-motion animations to About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import about from "../assets/about.jpeg";
 import { ABOUT } from "../constants";
+import { motion } from "framer-motion";
 
 const Sobre = () => {
   return (
@@ -12,13 +13,31 @@ const Sobre = () => {
           <img src={about} alt="about" className="rounded-3xl lg:-rotate-3" />
         </div>
         <div className="w-full px-2 lg:w-1/2">
-          <h2 className="text-4xl tracking-tighter lg:text-6xl">
+          <motion.h2
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.6, delay: 0.2 }}
+            className="text-4xl tracking-tighter lg:text-6xl"
+          >
             {ABOUT.header}
-          </h2>
-          <h2 className="mb-8 mt-1 h-2 w-36 bg-rose-300 lg:-rotate-3"></h2>
-          <p className="m-8 text-2xl leading-relaxed tracking-tight lg:max-w-xl">
+          </motion.h2>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.6, delay: 0.4 }}
+            className="mb-8 mt-1 h-2 w-36 bg-rose-300 lg:-rotate-3"
+          ></motion.div>
+          <motion.p
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.6, delay: 0.6 }}
+            className="m-8 text-2xl leading-relaxed tracking-tight lg:max-w-xl"
+          >
             {ABOUT.content}
-          </p>
+          </motion.p>
         </div>
       </div>
     </section>
