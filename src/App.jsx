3c0c1b235e7206c789f68279bfb639711e8b2a6e
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-07-31 11:08:58 -0300

    feat: add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Mission from "./components/Mission";
 import Expertise from "./components/Expertise";
 import Review from "./components/Review";
 import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Expertise />
         <Review />
         <ContactSection />
+        <Footer />
       </main>
     </>
   );
